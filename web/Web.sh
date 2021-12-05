d206329f9b176930645a4b5012908f4ad43cea4d
maithanhtan <maithanhtanbl@gmail.com>
2021-12-05 12:22:20 +0700

    Update Web.sh

--- web/Web.sh
+++ web/Web.sh
@@ -5,6 +5,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 sudo apt install docker-ce docker-compose wget -y
 cd /root
-wget -o /root/appsettings.json https://raw.githubusercontent.com/maithanhtan/script/main/web/appsettings.json
-wget -o /root/docker-compose.yml https://raw.githubusercontent.com/maithanhtan/script/main/web/docker-compose.yml
+wget -N https://raw.githubusercontent.com/maithanhtan/script/main/web/appsettings.json
+wget -N https://raw.githubusercontent.com/maithanhtan/script/main/web/docker-compose.yml
 HOSTNAME=$HOSTNAME docker-compose up -d
