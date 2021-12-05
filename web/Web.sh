10e1c66f1272fc43e71e9eb9e928c362d6bcab1e
maithanhtan <maithanhtanbl@gmail.com>
2021-12-05 12:08:56 +0700

    Update Web.sh

--- web/Web.sh
+++ web/Web.sh
@@ -4,7 +4,7 @@ sudo apt update && sudo apt install apt-transport-https ca-certificates curl sof
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 sudo apt install docker-ce docker-compose wget -y
-cd root
+cd /root
 wget https://raw.githubusercontent.com/maithanhtan/script/main/web/appsettings.json
 wget https://raw.githubusercontent.com/maithanhtan/script/main/web/appsettings.json
 HOSTNAME=$HOSTNAME docker-compose up -d
