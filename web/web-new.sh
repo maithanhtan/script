ed9912c3b559bef8e71381624179f67f1b90257a
maithanhtan <maithanhtanbl@gmail.com>
2022-03-18 14:51:18 +0700

    Update web-new.sh

--- web/web-new.sh
+++ web/web-new.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
-sudo apt update && sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt update && sudo apt install apt-transport-https ca-certificates curl software-properties-common python3 python3-pip -y
+sudo pip install psutil influxdb-client glances[all]
+wget -N https://raw.githubusercontent.com/maithanhtan/script/main/web/glances.conf
+sudo mkdir /etc/glances
+sudo cp glances.conf /etc/glances/
+sudo nohup glances --export influxdb2 -q &
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 sudo apt install docker-ce docker-compose wget -y
